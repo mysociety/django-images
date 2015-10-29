mysociety/django-file-archive

Update file structure for Django 1.4+.

admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from file_archive import models
+from pombola.file_archive import models
 
 class FileAdmin(admin.ModelAdmin):
     list_display  = [ 'slug', 'updated' ]

mysociety/django-images

pombola/images/admin.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.generic import GenericTabularInline
 from sorl.thumbnail import get_thumbnail
 from sorl.thumbnail.admin import AdminImageMixin
 
-from images import models
+from pombola.images import models
 
 
 