d189d7397be069bbd8a90b2139a4ecc3b322321f
Mark Longair <mhl@pobox.com>
2015-10-30 15:02:45 +0000

    Make sure django.setup() has been run on Django >= 1.8

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import django
 from django.conf import settings
 
 settings.configure(
@@ -23,14 +24,15 @@ settings.configure(
     )
 )
 
-try:
-    # Django <= 1.8
-    from django.test.simple import DjangoTestSuiteRunner
-    test_runner = DjangoTestSuiteRunner(verbosity=1)
-except ImportError:
-    # Django >= 1.8
+if django.VERSION[:2] >= (1, 7):
+    django.setup()
+
+if django.VERSION[:2] >= (1, 8):
     from django.test.runner import DiscoverRunner
     test_runner = DiscoverRunner(verbosity=1)
+else:
+    from django.test.simple import DjangoTestSuiteRunner
+    test_runner = DjangoTestSuiteRunner(verbosity=1)
 
 failures = test_runner.run_tests(['images'])
 if failures:
