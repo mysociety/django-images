f829602259f61956461a9eb4e543299174e6b0e8
Mark Longair <mhl@pobox.com>
2015-10-30 14:39:35 +0000

    Add a script for running tests standalone
    This is based on the suggested script here:
    
      http://stackoverflow.com/a/12260597/223092

---
+++ runtests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    },
+    ROOT_URLCONF='images.urls',
+    INSTALLED_APPS=(
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        'sorl.thumbnail',
+        'images',
+    )
+)
+
+try:
+    # Django <= 1.8
+    from django.test.simple import DjangoTestSuiteRunner
+    test_runner = DjangoTestSuiteRunner(verbosity=1)
+except ImportError:
+    # Django >= 1.8
+    from django.test.runner import DiscoverRunner
+    test_runner = DiscoverRunner(verbosity=1)
+
+failures = test_runner.run_tests(['images'])
+if failures:
+    sys.exit(failures)
