178fe1b13145027de4908da36cb0e1deda68f18d
Mark Longair <mhl@pobox.com>
2015-10-30 14:48:31 +0000

    Add a setup.py for building a Python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = "mysociety-django-images",
+    version = "0.0.1",
+    packages = find_packages(),
+    author = "Edmund von der Burg",
+    author_email = "evdb@ecclestoad.co.uk",
+    description = "Attach images to any Django model, with helpful admin",
+    license = "AGPL",
+    keywords = "django images",
+    install_requires = [
+        'Django',
+        'sorl-thumbnail',
+        'Pillow',
+    ]
+)
